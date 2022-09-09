d4ef261031101d006798709bdd19a8e6e7c7c40c
Sina Moradian <sinacsharp@gmail.com>
2022-09-09 13:20:14 +0430

    Add Swiper Slider & Refactor

--- js/index.js
+++ js/index.js
@@ -23,3 +23,38 @@ function closeSidebar() {
     sidebar.style.transform = "translateX(100%)";
     body.style.overflowY = "unset";
 }
+
+
+/*--------------
+ SLIDER SCRIPTS
+--------------*/
+
+var swiper = new Swiper(".slider-container", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    // slidesPerGroup: 3,
+    centerSlide: true,
+    fade: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        }
+    }
+});
