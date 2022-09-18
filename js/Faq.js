f02af5296bb1aa070eaa673afa0ee71b4953b1a0
Sina Moradian <sinacsharp@gmail.com>
2022-09-18 05:29:51 +0430

    Add Accordion functionality

---
+++ js/Faq.js
@@ -0,0 +1,18 @@
+const accordionBtn = document.querySelectorAll(".accordion");
+
+/* Event Listeners */
+accordionBtn.forEach(btn => {
+    btn.addEventListener("click", toggleAccordionContent);
+})
+
+/* Functions */
+function toggleAccordionContent(e) {
+    e.target.classList.toggle("active-accordion");
+    const accordionContent = e.target.nextElementSibling;//get the accordion content next to clicked btn
+    if (accordionContent.style.maxHeight) {//has max-height
+        accordionContent.style.maxHeight = null;
+    } else {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + "px"; //Add content height including border and padding to the max-height
+    }
+}
+
