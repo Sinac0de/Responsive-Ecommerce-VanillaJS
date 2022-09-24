8d9da6f3a5abeef2a4ae3ecedb01cbb295c762bb
Sina Moradian <sinacsharp@gmail.com>
2022-09-24 03:41:11 +0330

    Fix Check if there isn't back btn before appending

--- js/SProduct.js
+++ js/SProduct.js
@@ -91,14 +91,18 @@ export default class SProduct {
         }
 
         /*-----GoBackBtn----*/
-        const goBackBtn = document.createElement("a");
-        goBackBtn.setAttribute("href", window.location.pathname);
-        goBackBtn.innerHTML = `<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M21 11L6.414 11 11.707 5.707 10.293 4.293 2.586 12 10.293 19.707 11.707 18.293 6.414 13 21 13z"></path></svg>`;
-        document.querySelector("header").prepend(goBackBtn);
+        if (!document.querySelector(".go-back-btn")) {//if there isn't any back button
+            const goBackBtn = document.createElement("a");
+            goBackBtn.setAttribute("href", window.location.pathname);
+            goBackBtn.classList.add("go-back-btn");
+            goBackBtn.innerHTML = `<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M21 11L6.414 11 11.707 5.707 10.293 4.293 2.586 12 10.293 19.707 11.707 18.293 6.414 13 21 13z"></path></svg>`;
+            document.querySelector("header").prepend(goBackBtn);
 
-        goBackBtn.style.padding = "0.4em";
-        goBackBtn.style.fontSize = "1.2rem";
-        goBackBtn.style.marginLeft = "0.3em";
+            goBackBtn.style.padding = "0.4em";
+            goBackBtn.style.fontSize = "1.2rem";
+            goBackBtn.style.marginLeft = "0.3em";
+        }
+
 
         //set the history to store or home page
         window.history.pushState(null, null, `${window.location.href}`);
