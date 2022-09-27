93e8c1aad83d2b0c3440f1eab6863385acabde67
Sina Moradian <sinacsharp@gmail.com>
2022-09-27 23:52:15 +0330

    Transfer cartIsEmpty method to CartLogic

--- js/CartLogic.js
+++ js/CartLogic.js
@@ -1,5 +1,4 @@
 import Storage from "./Storage.js";
-import { cartIsEmpty } from "./cart.js";
 
 export default class CartLogic {
 
@@ -52,7 +51,7 @@ export default class CartLogic {
             product.remove();
             /*--Check if cart is empty--*/
             if (CartLogic.totalQuantity() < 1) {
-                cartIsEmpty();
+                this.cartIsEmpty();
             } else {
                 const cartTotalPrice = document.getElementById("total-price-value");
                 cartTotalPrice.textContent = this.totalPrice();
@@ -135,13 +134,29 @@ export default class CartLogic {
         allCartProducts.forEach(cartProduct => {
             cartProduct.remove();
         });
-        cartIsEmpty();
+        this.cartIsEmpty();
 
 
         totalQuantities.forEach(tQuantity => {
             tQuantity.style.display = "none";
         });
     }
+
+    //if cart is empty
+    static cartIsEmpty() {
+    //main content of cart page 
+    const cartContent = document.getElementById("cart");
+    cartContent.innerHTML = "";
+    //create cart is empty content
+    let cartEmpty = document.createElement("div");
+    cartEmpty.classList.add("cart-is-empty");
+    /*------Cart is Empty content-----*/
+    cartEmpty.innerHTML = `<svg id="cart-is-empty-icon" stroke="currentColor" fill="currentColor" stroke-width="0" version="1.2" baseProfile="tiny" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g><path d="M20.756 5.345c-.191-.219-.466-.345-.756-.345h-13.819l-.195-1.164c-.08-.482-.497-.836-.986-.836h-2.25c-.553 0-1 .447-1 1s.447 1 1 1h1.403l1.86 11.164.045.124.054.151.12.179.095.112.193.13.112.065c.116.047.238.075.367.075h11.001c.553 0 1-.447 1-1s-.447-1-1-1h-10.153l-.166-1h11.319c.498 0 .92-.366.99-.858l1-7c.041-.288-.045-.579-.234-.797zm-1.909 1.655l-.285 2h-3.562v-2h3.847zm-4.847 0v2h-3v-2h3zm0 3v2h-3v-2h3zm-4-3v2h-3l-.148.03-.338-2.03h3.486zm-2.986 3h2.986v2h-2.653l-.333-2zm7.986 2v-2h3.418l-.285 2h-3.133z"></path><circle cx="8.5" cy="19.5" r="1.5"></circle><circle cx="17.5" cy="19.5" r="1.5"></circle></g></svg>
+    <h2>Cart is empty!</h2>`;
+    //render to the Dom
+    cartContent.append(cartEmpty);
+    cartContent.classList.add("cart-is-empty");
+}
 
     //calculate total cart price
     static totalPrice() {
--- js/cart.js
+++ js/cart.js
@@ -12,8 +12,11 @@ document.addEventListener("DOMContentLoaded", setup);
 
 function setup() {
 
-    if (cart.length > 0) {
+    /*--render cart products--*/
+    if (cart.length > 0) {//cart isn't empty
         let productsDiv = "";
+
+        //create cart products
         cart.forEach(cProduct => {
             const { id, title, price, quantity, image } = cProduct;
             const productPrice = (quantity * price).toFixed(2);
@@ -71,21 +74,10 @@ function setup() {
         cartContent.classList.remove("cartIsEmpty");
 
 
-    } else {
-        cartIsEmpty();
+    } else {//cart is empty
+        CartLogic.cartIsEmpty();
     }
 
 }
 
-//if cart is empty
-export function cartIsEmpty() {
-    cartContent.innerHTML = "";
-    let cartEmpty = document.createElement("div");
-    cartEmpty.classList.add("cart-is-empty");
-    /*------Cart is Empty content-----*/
-    cartEmpty.innerHTML = `<svg id="cart-is-empty-icon" stroke="currentColor" fill="currentColor" stroke-width="0" version="1.2" baseProfile="tiny" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g><path d="M20.756 5.345c-.191-.219-.466-.345-.756-.345h-13.819l-.195-1.164c-.08-.482-.497-.836-.986-.836h-2.25c-.553 0-1 .447-1 1s.447 1 1 1h1.403l1.86 11.164.045.124.054.151.12.179.095.112.193.13.112.065c.116.047.238.075.367.075h11.001c.553 0 1-.447 1-1s-.447-1-1-1h-10.153l-.166-1h11.319c.498 0 .92-.366.99-.858l1-7c.041-.288-.045-.579-.234-.797zm-1.909 1.655l-.285 2h-3.562v-2h3.847zm-4.847 0v2h-3v-2h3zm0 3v2h-3v-2h3zm-4-3v2h-3l-.148.03-.338-2.03h3.486zm-2.986 3h2.986v2h-2.653l-.333-2zm7.986 2v-2h3.418l-.285 2h-3.133z"></path><circle cx="8.5" cy="19.5" r="1.5"></circle><circle cx="17.5" cy="19.5" r="1.5"></circle></g></svg>
-    <h2>Cart is empty!</h2>`;
-    //render to the Dom
-    cartContent.append(cartEmpty);
-    cartContent.classList.add("cart-is-empty");
-}
+
